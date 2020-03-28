02b28f3885f37b639d2cb09b17db8b4a30fda5ba
黑米粥工作室 <62678724+Heymmy-Zhou@users.noreply.github.com>
2020-03-28 14:59:53 +0800

    Add files via upload

---
+++ Heymmy-Zhou_Studio_2048_V1.2_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef HEYMMY-ZHOU_STUDIO_2048_V1_2_PRIVATE_H
+#define HEYMMY-ZHOU_STUDIO_2048_V1_2_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.2.0.0"
+#define VER_MAJOR	1
+#define VER_MINOR	2
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"黑米粥工作室 Heymmy-Zhou Studio"
+#define FILE_VERSION	"1.2.0.0"
+#define FILE_DESCRIPTION	"黑米粥工作室 2048 V1.2 PC版"
+#define INTERNAL_NAME	"Heymmy-Zhou Studio 2048"
+#define LEGAL_COPYRIGHT	"版权所有（C）2020 黑米粥工作室"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"黑米粥工作室 2048"
+#define PRODUCT_VERSION	"1.2.0.0"
+
+#endif /*HEYMMY-ZHOU_STUDIO_2048_V1_2_PRIVATE_H*/
